7bc770a98a66eee6cd10a85a2f340282d9ae1336
lpenin <peninlouis@gmail.com>
2018-03-20 15:52:05 +0100

    decla des diag

--- Sequentiel.f90
+++ Sequentiel.f90
@@ -7,8 +7,8 @@ Program Projet
 Integer:: Nx, Ny
 Real*8:: Lx, Ly, dx, dy, dt, tfinal, D
 Real*8,dimension(:,:),allocatable:: U_0, U, Mat_f
-Real*8,dimension(:),allocatable::IA,JA,AA
-Real*8,Parameter:: coeff_a,coeff_b,coeffe_c
+Real*8,dimension(:),allocatable::A,B1,B2,C1,C2
+Real*8,Parameter:: coeff_a,coeff_b,coeff_c
 
 
 ! INITIALISATION
@@ -22,7 +22,7 @@ READ(unit,*) Nx, Ny
 CLOSE(unit)
 
 Allocate(U_0(0:Nx+1,0:Ny+1), U(0:Nx+1,0:Ny+1), Mat_f(0:Nx+1,0:Ny+1))
-Allocate(AA(0:5*Nx*Ny-2*Nx-2*Ny), JA(0:5*Nx*Ny-2*Nx-2*Ny), IA(0:Nx*Ny)) !expliquer d'ou vient la taille des vecteurs
+Allocate(A(1:Nx*Ny), B1(1:Nx*Ny), B2(1:Nx*Ny), C1(1:Nx*Ny), C2(1:Nx*Ny)) !expliquer d'ou vient la taille des vecteurs
 
 A=1.0d0+ 2.0d0*D*dt/(dy*dy) + 2.0d0*D*dt/(dx*dx)
 B= -1.0d0*D*dt/(dx*dx)
