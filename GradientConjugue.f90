da195bc873a49470f907e79acf3628d7f5bc3447
CONSTANT Benoit <bconstant001@septantetix.pedago.ipb.fr>
2018-03-27 17:21:33 +0200

    plus que le gradiant les ptits pote

--- GradientConjugue.f90
+++ GradientConjugue.f90
@@ -2,22 +2,26 @@ Module Gradient
 use fonction
 
 contains
-  function GC(A,B1,B2,C1,C2,b,X,size)
+  subroutine GC(A,B1,B2,C1,C2,b,X,size)
     integer,intent(in)::size
-    real*8,dimension(:),intent(in)::b,A,B1,B2,C1,C2,X
-    real*8,dimension(:),allocatable::Ap,Ax,GC,rk,rk1,p
+    real*8,dimension(:),intent(in)::b,A,B1,B2,C1,C2
+    real*8,dimension(:),intent(inout)::X
+    real*8,dimension(:),allocatable::Ap,Ax,rk,rk1,p
     real*8::alpha,beta
     integer::k
 
-    Allocate(GC(size), Ax(size), Ap(size), rk(size), rk1(size), p(size))
+    Allocate( Ax(size), Ap(size), rk(size), rk1(size), p(size))
     call Mat_mul_creux(A,B1,B2,C1,C2,Ax,X)
     rk=b-Ax
     p=rk
     k=0
     call Mat_mul_creux(A,B1,B2,C1,C2,Ap,p)
-    do while (sqrt(dot_product(rk,rk))<0.000001d0)
+
+    do while (sqrt(dot_product(rk,rk))>0.001d0)
+      call Mat_mul_creux(A,B1,B2,C1,C2,Ap,p)
       alpha=dot_product(rk,rk)/(dot_product(p,Ap))   !A*p
-      GC=GC+alpha*p
+      X=X+alpha*p
+      !print*,"alpha =", alpha
       rk1=rk-alpha*Ap
 
       beta=dot_product(rk1,rk1)/dot_product(rk,rk)
@@ -26,6 +30,8 @@ contains
       rk=rk1
     end do
     deallocate(Ax,Ap,rk,rk1,p)
-  end function
+  end subroutine
+
+
 
 end Module
--- Sequentiel.f90
+++ Sequentiel.f90
@@ -7,7 +7,7 @@ Program Projet
 ! PARAMETRE DU SYSTEME
 Integer:: Nx, Ny, i,Maxiter,j,kt,k,n
 Real*8:: Lx, Ly, dx, dy, dt, tfinal, D, t
-Real*8,dimension(:),allocatable:: U_0, U, Mat_f,X
+Real*8,dimension(:),allocatable:: U_0, U, Mat_f,Ur
 Real*8,dimension(:),allocatable::A,B1,B2,C1,C2
 Real*8:: coeff_a,coeff_b,coeff_c
 real:: t1,t2
@@ -29,17 +29,22 @@ READ(10,*) tfinal
 CLOSE(10)
 
 Allocate(U_0(1:Nx*Ny), U(1:Nx*Ny), Mat_f(1:Nx*Ny))
-Allocate(A(1:Nx*Ny), B1(1:Nx*Ny), B2(1:Nx*Ny), C1(1:Nx*Ny), C2(1:Nx*Ny),X(1:Nx*Ny)) !expliquer d'ou vient la taille des vecteurs
+Allocate(A(1:Nx*Ny), B1(1:Nx*Ny), B2(1:Nx*Ny), C1(1:Nx*Ny), C2(1:Nx*Ny),Ur(1:Nx*Ny)) !expliquer d'ou vient la taille des vecteurs
 
+dx=Lx/(Nx+1)
+dy=Ly/(Ny+1)
+dt=0.9d0*dx*dx/(2.d0*D)
 coeff_a=1.0d0+ 2.0d0*D*dt/(dy*dy) + 2.0d0*D*dt/(dx*dx)
 coeff_b= -1.0d0*D*dt/(dx*dx)
 coeff_c= -1.0d0*D*dt/(dy*dy)
 
 n=Nx*Ny
 !! matrice
+B1=0.d0
+B2=0.d0
 A=coeff_a
 do i=1,n
-  if (i<n-Nx) then
+  if (i<n-Nx+1) then
     B1(i)=coeff_b
   end if
   if (i>Nx)then
@@ -48,20 +53,33 @@ do i=1,n
 end do
 
 
+
+C1=coeff_c
+C2=coeff_c
+do i=1,Nx
+ C1(i*Nx)=0.d0
+ C2((i-1)*Nx+1)=0.d0
+end do
+
+
+
+
 Maxiter= int(tfinal/dt)
 
 !On donne la condition initiale à U
 U(:)= 0.0d0
-
+mat_f=0.d0
   DO kt = 1, Maxiter
     ! constructio n de Mat_f
+
     k=1
     DO j=1,Ny
       DO i=1,Nx
+
         Mat_f(k)=Mat_f(k)+f(i*dx,j*dy,kt)
 
       IF (i==1) THEN
-        Mat_f(k)=Mat_f(k)-coeff_c*h((i-1)*dx,0.d0,kt)
+        Mat_f(k)=Mat_f(k)-coeff_b*h((i-1)*dx,0.d0,kt)
 
       ELSE if (i==Nx)THEN
         Mat_f(k)=Mat_f(k)-coeff_b*g((i-1)*dx,Ny*dy,kt)
@@ -72,26 +90,48 @@ U(:)= 0.0d0
       ELSE IF (j==0) THEN
         Mat_f(k)=Mat_f(k)-coeff_c*h(i*dx,(Ny-1)*dy,kt)
 
+      ENDIF
       k=k+1
 
-
-    ENDIF
-
+      END DO
     END DO
+
+
+  call GC(A,B1,B2,C1,C2,Mat_f,U,n)
+
+
+
   END DO
-
-  X=GC(A,B1,B2,C1,C2,Mat_f,X,n)
-
-  END DO
-
-print*, X
+!  print*,u
 
 
+  !!vecteur solution
+  k=1
+  ur=0.d0
+  DO j=1,Ny
+    DO i=1,Nx
+
+      ur(k)=i*dx*(1.d0-i*dx)*j*dy*(1.d0-j*dy)
+      k=k+1
+
+    end do
+  end do
+
+
+  k=1
+ DO j=1,NY
+   do i=1,Nx
+   PRINT*, i*dx, j*dy, u(k)
+   k=k+1
+ END DO!
+ end do
 
 
 
-Deallocate(U_0,U,Mat_f,X)
+
+
+Deallocate(U_0,U,Mat_f)
 call CPU_TIME( t2 )
-print *,t2 - t1
+! print *,t2 - t1
 
 end program
