e4b702e3785c08b9108e6eef944c6cee96797264
Adumaye <dumaye.antoine@gmail.com>
2018-04-12 19:21:57 +0200

    recommente et sans trop de barriere

--- fonction_parallele.f90
+++ fonction_parallele.f90
@@ -2,77 +2,90 @@ Module Fonction_para
   Implicit NONE
 
 Contains
+  !!!   Calcul la charge de tache, nbr de ligne de l'équation, de chaqu'un des proc   !!!
   subroutine charge(me,N,Np,i1,iN)
-    integer, intent(in):: me, N, Np
-    integer,intent(out)::i1, iN
+    Integer, intent(in):: me, N, Np
+    Integer,intent(out)::i1, iN
     i1=me*N/Np+1
     iN=(me+1)*N/Np
   end subroutine
 
+
+  !!!   Calcul les lignes i1 a iN du produit A*U a partir des 5 vecteurs diagonaux   !!!
   subroutine Mat_mul_para(A,B1,B2,C1,C2,X,U,Nx,i1,iN) !calcul AU=X
-    real*8,dimension(:),intent(in)::A,B1,B2,C1,C2,U
-    integer,intent(in)::Nx,i1,iN
-    integer::n,i
-    real*8,dimension(:), allocatable,intent(out):: X
+    Real*8,dimension(:),intent(in)::A,B1,B2,C1,C2,U
+    Integer,intent(in)::Nx,i1,iN
+    Integer::n,i
+    Real*8,dimension(:),allocatable,intent(out):: X
     n=size(A)
     Allocate(X(n))
     X=0.d0
-    do i=i1,iN
+    Do i=i1,iN
 
-      if (i==1) then
+      If (i==1) then
         X(1)=A(1)*U(1)+C1(1)*U(2)+B1(1)*U(1+Nx)
-      end if
-      if (i>1 .and. i<Nx+1) then
+      End if
+      If (i>1 .and. i<Nx+1) then
         X(i)=A(i)*U(i)+C2(i)*U(i-1)+C1(i)*U(i+1)+B1(i)*U(i+Nx)
-      end if
-      if (i>Nx .and. i<N-Nx) then
+      End if
+      If (i>Nx .and. i<N-Nx) then
         X(i)=A(i)*U(i)+C2(i)*U(i-1)+C1(i)*U(i+1)+B1(i)*U(i+Nx)+B2(i)*U(i-Nx)
-      end if
-      if (i>N-Nx-1 .and. i<N) then
+      End if
+      If (i>N-Nx-1 .and. i<N) then
         X(i)=A(i)*U(i)+C1(i)*U(i+1)+C2(i)*U(i-1)+B2(i)*U(i-Nx)
-      end if
-      if (i==n) then
+      End if
+      If (i==n) then
         X(N)=A(N)*U(N)+C2(N-1)*U(N-1)+B2(N-Nx-1)*U(N-Nx-1)
-      end if
-    end do
-  end subroutine
+      End if
+    End do
+  End subroutine
 
+
+  !!!   Gradient conjugue parallelise a partir de votre algorithme   !!!
   subroutine GC_para(A,B1,B2,C1,C2,Mat_f,U,n,Nx)
     include 'mpif.h'
+    Integer,intent(in)::n, Nx
     Real*8,dimension(:),intent(in)::A,B1,B2,C1,C2, Mat_f
     Real*8,dimension(:),intent(inout)::U
-    Integer::n, Nx, i, j, l
-    REAL*8:: epsilon, alpha, beta, residu, drl, dwl, betal, residul, dr, dw, res_sum
     Real*8,dimension(:),allocatable:: U_0, r, d1, W, W1, Up
-    integer::me,Np,statinfo,i1,iN
-    integer,dimension(MPI_STATUS_SIZE)::status
+    Real*8:: epsilon, alpha, beta, residu, drl, dwl, betal, residul, dr, dw, res_sum
+    Integer::i, j, l
+
+    Integer::me,Np,statinfo,i1,iN
+    Integer,dimension(MPI_STATUS_SIZE)::status
 
     call MPI_comm_rank(MPI_comm_world,me,Statinfo)
     call MPI_comm_size(MPI_comm_world,Np,Statinfo)
 
     Allocate(W(1:n), W1(1:n), U_0(1:n), r(1:n), d1(1:n), Up(1:n))
 
-
-    ! Initialisation Gradient conjugue
     call charge(me,n,Np,i1,iN)
-    call Mat_mul_para(A,B1,B2,C1,C2,W,U,Nx,i1,iN) !W=AU
+    call Mat_mul_para(A,B1,B2,C1,C2,W,U,Nx,i1,iN) ! W=A*U
 
-    DO i=i1,iN
+    Do i=i1,iN
       U_0(i) = U(i)
       r(i)     = W(i) - Mat_f(i)
       d1(i)     = W(i) - Mat_f(i)
-    END DO
+    End do
 
     residu = 0.d0
     res_sum=0.d0
-    DO i=i1,iN
-      res_sum = res_sum + r(i)*r(i) !SUM(r*r)
-    ENDDO
-    call MPI_allreduce(res_sum,residu,1,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
+    Do i=i1,iN
+      residu= residu + r(i)*r(i)
+    End do
 
-    ! Boucle du Gradient conjugue
+    If (me==0) then
+      Do i=1,Np-1
+        call MPI_recv(res_sum,1,MPI_real8,i,10,MPI_COMM_WORLD,status,statinfo)
+        residu=residu+res_sum
+      End do
+    Else
+      call MPI_send(residu,1,MPI_real8,0,10,MPI_COMM_WORLD,statinfo)
+    End if
+    call MPI_bcast(residu,1,MPI_real8,0,MPI_comm_world,statinfo)
+
     l=1
-    DO WHILE ((l<10000).AND.( SQRT(residu) .ge. 0.0001 ))
+    Do while((l<10000).AND.( SQRT(residu) .ge. 0.0001 ))
       call MPI_allreduce(d1,w1,n,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
 
       call charge(me,n,Np,i1,iN)
@@ -82,66 +95,96 @@ Contains
       dwl = 0.0d0
       dr=0.d0
       dw=0.d0
-      DO i = i1, iN
+      Do i = i1, iN
         drl = drl + d1(i)*r(i)
         dwl = dwl + d1(i)*w(i)
-      ENDDO
+      End do
 
-      call MPI_allreduce(drl,dr,1,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
-      call MPI_allreduce(dwl,dw,1,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
+      If (me==0) then
+        Do i=1,Np-1
+          call MPI_recv(dr,1,MPI_real8,i,100,MPI_COMM_WORLD,status,statinfo)
+          call MPI_recv(dw,1,MPI_real8,i,200,MPI_COMM_WORLD,status,statinfo)
+          drl=drl+dr
+          dwl=dwl+dw
+        End do
+        alpha = drl/dwl
+      Else
+        call MPI_send(drl,1,MPI_real8,0,100,MPI_COMM_WORLD,statinfo)
+        call MPI_send(dwl,1,MPI_real8,0,200,MPI_COMM_WORLD,statinfo)
+      End if
+      call MPI_bcast(alpha,1,MPI_real8,0,MPI_comm_world,statinfo)
 
-      alpha = dr/dw
       call charge(me,n,Np,i1,iN)
-      DO i=i1,iN
+      Do i=i1,iN
         U_0(i) = U_0(i) - alpha*d1(i)
         r(i) = r(i) - alpha*W(i)
-      END DO
+      End do
       betal=0.d0
-      DO i=i1,iN
-        betal=betal+ r(i)*r(i)
-      ENDDO
-      call MPI_allreduce(betal/residu,beta,1,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
+      Do i=i1,iN
+        beta=beta+ r(i)*r(i)
+      End do
 
-      DO i=i1,iN
+      If (me==0) then
+        Do i=1,Np-1
+          call MPI_recv(betal,1,MPI_real8,i,300,MPI_COMM_WORLD,status,statinfo)
+          beta=betal+beta
+        End do
+      Else
+        call MPI_send(beta/residu,1,MPI_real8,0,300,MPI_COMM_WORLD,statinfo)
+      End if
+      call MPI_bcast(beta,1,MPI_real8,0,MPI_comm_world,statinfo)
+
+      Do i=i1,iN
         d1(i) = r(i) + beta*d1(i)
-      ENDDO
+      End do
       residu = 0.d0
       res_sum=0.d0
-      DO i=i1,iN
+      Do i=i1,iN
         res_sum    = res_sum+r(i)*r(i)
-      ENDDO
-      call MPI_allreduce(res_sum,residu,1,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
+      End do
+
+      If (me==0) then
+        Do i=1,Np-1
+          call MPI_recv(res_sum,1,MPI_real8,i,20,MPI_COMM_WORLD,status,statinfo)
+          residu=residu+res_sum
+        End do
+      Else
+        call MPI_send(residu,1,MPI_real8,0,20,MPI_COMM_WORLD,statinfo)
+      End if
+      call MPI_bcast(residu,1,MPI_real8,0,MPI_comm_world,statinfo)
+
       l=l+1
-    ENDDO
+    End do
 
     u=0.d0
-    DO i=i1,iN
+    Do i=i1,iN
       U(i)=U_0(i)
-    ENDDO
+    End do
 
-    ! Fin boucle en temps
-    call MPI_allreduce(u,up,n,MPI_real8,MPI_sum,MPI_comm_world,statinfo)
+    call MPI_allreduce(u,up,n,MPI_real8,MPI_sum,MPI_comm_world,statinfo) !!! REGLER LE PB DE MATMUL PARA ET ON SUPRIME
     u=up
   end subroutine
 
+
+  !!!   Fonction aux limites    !!!
   function f(x,y,t)
-    real*8,intent(in):: x, y, t
-    real*8:: f
+    Real*8,intent(in):: x, y, t
+    Real*8:: f
     f=2.0d0*(y - y*y + x - x*x)
     !f=sin(x)+cos(y)
     !f=exp( -1.0d0*pow((x-Lx/2.0d0),2) )*exp( -1.0d0*pow((y-Ly/2.0d0),2) )*cos( 2*atan(1.0D0)*t )
   end function
 
   function g(x,y,t)
-    real*8,intent(in):: x, y, t
-    real*8:: g
+    Real*8,intent(in):: x, y, t
+    Real*8:: g
     g=0.0d0
     !g=sin(x)+cos(y)
   end function
 
   function h(x,y,t)
-    real*8,intent(in):: x, y, t
-    real*8:: h
+    Real*8,intent(in):: x, y, t
+    Real*8:: h
     h=0.0d0
     !h=sin(x)+cos(y)
   end function
--- parallele.f90
+++ parallele.f90
@@ -1,9 +1,9 @@
 PROGRAM para
   use Fonction_para
-  IMPLICIT NONE
-  include 'mpif.h'
-  integer::me,Np,statinfo,i1,iN
-  integer,dimension(MPI_STATUS_SIZE)::status
+  Implicit NONE
+  Include 'mpif.h'
+  Integer::me,Np,statinfo,i1,iN
+  Integer,dimension(MPI_STATUS_SIZE)::status
 
   ! PARAMETRE DU SYSTEME
   Integer:: Nx, Ny, i,Maxiter,j,kt,k,n,l, Nl
@@ -15,7 +15,7 @@ PROGRAM para
 
   !!!   INITIALISATION    !!!
 
-  ! Lis fichier data
+  ! Lis le fichier data
   OPEN(10,file='data', form='formatted', status='old')
   READ(10,*)
   READ(10,*) Lx, Ly
@@ -27,11 +27,11 @@ PROGRAM para
   READ(10,*) tfinal
   CLOSE(10)
 
-  ! Definition des matices
+  ! Definition des matrices
   Allocate(U(1:Nx*Ny), Mat_f(1:Nx*Ny), Ur(1:Nx*Ny))
-  Allocate(A(1:Nx*Ny), B1(1:Nx*Ny), B2(1:Nx*Ny), C1(1:Nx*Ny), C2(1:Nx*Ny)) 
+  Allocate(A(1:Nx*Ny), B1(1:Nx*Ny), B2(1:Nx*Ny), C1(1:Nx*Ny), C2(1:Nx*Ny))
 
-  ! Création des variables utiles
+  ! Creation des variables utiles
   dx=Lx/(Nx+1)
   dy=Ly/(Ny+1)
   dt=0.9d0*dx*dx/(4.d0*D)
@@ -45,71 +45,72 @@ PROGRAM para
 
   B1=0.d0
   B2=0.d0
-  do i=1,n
-    if (i<n-Nx+1) then
+  Do i=1,n
+    If (i<n-Nx+1) then
       B1(i)=coeff_b
-    end if
-    if (i>Nx)then
+    End if
+    If (i>Nx)then
       B2(i)=coeff_b
-    end if
-  end do
+    End if
+  End do
 
   C1=coeff_c
   C2=coeff_c
-  do i=1,Nx
+  Do i=1,Nx
     C1(i*Nx)=0.d0
     C2((i-1)*Nx+1)=0.d0
-  end do
+  End do
 
   ! Initialisation du vecteur U
   U(:)= 0.0d0
   !!!   FIN INITIALISATION    !!!
 
 
-
-  !!!   BOUCLE EN TEMPS   !!!
-
   ! Debut de parallelisation
   Call MPI_init(statinfo)
   call MPI_comm_rank(MPI_comm_world,me,Statinfo)
   call MPI_comm_size(MPI_comm_world,Np,Statinfo)
 
-  ! Chrono
-  if (me==0) then
+  ! Demmarage du chronometre
+  If (me==0) then
     call CPU_TIME( t1 )
-  end if
+  End if
 
+
+  !!!   BOUCLE EN TEMPS   !!!
   Maxiter= int(tfinal/dt)
-  DO kt = 1, Maxiter
+
+  Do kt = 1, Maxiter
     ! Construction de Mat_f, le membre de droite
     mat_f=0.d0
     call charge(me,n,Np,i1,iN)
 
-    do k=i1,iN
+    Do k=i1,iN
       i=mod(k-1,Nx)+1
-      j=k/Nx
-      j=int(j)+1
+      j=k/Nx+1
       Mat_f(k)=U(k)+f(i*dx,j*dy,kt*dt)*dt
-      IF (i==1) THEN
+      If (i==1) then
         Mat_f(k)=Mat_f(k)-coeff_b*h((i-1)*dx,0.d0,dt*kt)
-      end if
-      if (i==Nx)THEN
+      Endif
+      If (i==Nx)then
         Mat_f(k)=Mat_f(k)-coeff_b*g((i-1)*dx,Ny*dy,dt*kt)
-      endif
-      IF (j==1) THEN
+      Endif
+      If (j==1) then
         Mat_f(k)=Mat_f(k)-coeff_c*h(i*dx,0.d0,dt*kt)
-      endif
-      IF (j==Ny) THEN
+      Endif
+      If (j==Ny) then
         Mat_f(k)=Mat_f(k)-coeff_c*h(i*dx,(Ny-1)*dy,dt*kt)
-      end if
-    END DO
+      Endif
+    End do
 
     call GC_para(A,B1,B2,C1,C2,Mat_f,U,n,Nx)
 
     t = t + dt
-  ENDDO
+  End do
+  !!!   FIN BOUCLE EN TEMPS   !!!
 
-  !!vecteur solution si f=y-y²+x-x² et g=h=0
+
+  ! ! Vecteur solution si f=y-y²+x-x² et g=h=0
   ! k=1
   ! ur=0.d0
   ! DO j=1,Ny
@@ -119,16 +120,12 @@ PROGRAM para
   !   end do
   ! end do
 
-  ! if (me==0) then
-  !   do i=1,n
-  !     print*, u(i)
-  !   end do
-  ! end if
-
-  Deallocate(U,Mat_f,Ur,A,B1,B2,C1,C2)
-  if (me==0) then
+  ! Fin du chronometre
+  If (me==0) then
     call CPU_TIME( t2 )
     print *,t2 - t1
-  end if
+  End if
+
+  Deallocate(U,Mat_f,Ur,A,B1,B2,C1,C2)
   call MPI_finalize(statinfo)
 End program
